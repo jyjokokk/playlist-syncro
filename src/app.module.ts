20ccbcadf19732a73514270e33df7eb8bed57f47
Jyrki Kokkola <j.j.kokkola@gmail.com>
2023-02-26 10:49:25 +0200

    linkedList

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { PlaylistModule } from './playlist/playlist.module';
 import { UtilsModule } from './utils/utils.module';
 import { LocalDatabaseApiModule } from './local-database-api/local-database-api.module';
+import { LinkedListModule } from './linked-list/linked-list.module';
 
 @Module({
-  imports: [PlaylistModule, UtilsModule, LocalDatabaseApiModule],
+  imports: [
+    PlaylistModule,
+    UtilsModule,
+    LocalDatabaseApiModule,
+    LinkedListModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
