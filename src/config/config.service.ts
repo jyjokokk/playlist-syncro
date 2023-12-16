a3a8f42104f735481e6f5b7142e76ac06d6502df
Jyrki Kokkola <j.j.kokkola@gmail.com>
2023-12-16 16:36:26 +0200

    environment utils

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,17 +1,19 @@
 import { parseBoolean } from '../utils/parseBoolean.util'
 
 class ConfigService {
-  config() {
+  getConfig() {
     const PORT = parseInt(process.env.PORT)
     const IS_LOCAL = parseBoolean(process.env.IS_LOCAL)
     const IS_DEVELOPMENT = parseBoolean(process.env.IS_DEVELOPMENT)
     const IS_PRODUCTION = parseBoolean(process.env.IS_PRODUCTION)
+    const HOSTNAME = process.env.HOSTNAME
 
     return {
       PORT,
       IS_LOCAL,
       IS_DEVELOPMENT,
-      IS_PRODUCTION
+      IS_PRODUCTION,
+      HOSTNAME
     }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
 import 'dotenv/config'
 import configService from './config/config.service'
 
-const config = configService.config()
+const config = configService.getConfig()
 
-export function add(a: number, b: number): number {
-  return a + b
-}
-
-const joku = {
-a: 'avs',
-  bool: true
-}
-
-console.log(   'rumaa koodia', config.IS_LOCAL)
-
-console.log('PORT', config.PORT)
 console.log('PORT', config.PORT)
