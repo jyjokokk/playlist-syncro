eff4db2b8349bf1eadcb15349a5f24404add5244
Jyrki Kokkola <j.j.kokkola@gmail.com>
2024-01-01 23:34:44 +0200

    coverage

--- src/utils/api-http/url-encode.spec.ts
+++ src/utils/api-http/url-encode.spec.ts
@@ -4,10 +4,28 @@ const object1 = {
   client_id: 'alfa1234',
   client_name: 'new_user'
 }
+const object2 = {
+  str_with_space: 'two spaces here',
+  with_numbers: '1 with 2 numbers'
+}
+const object3 = {
+  numbers: 10,
+  string: 'words'
+}
 
 describe('urlencodeObject', () => {
-  it('makes an urlencoded string of a <string, string | number> object', () => {
+  it('urlencoded alphanumeric keys and values of an object', () => {
     const r = urlencodeObject(object1)
     expect(r).toStrictEqual('client_id=alfa1234&client_name=new_user')
+  })
+  it('handles spaces and other special characters', () => {
+    const r = urlencodeObject(object2)
+    expect(r).toStrictEqual(
+      'str_with_space=two%20spaces%20here&with_numbers=1%20with%202%20numbers'
+    )
+  })
+  it('handles numbers', () => {
+    const r = urlencodeObject(object3)
+    expect(r).toStrictEqual('numbers=10&string=words')
   })
 })
--- src/utils/read-and-write-files.spec.ts
+++ src/utils/read-and-write-files.spec.ts
@@ -4,7 +4,13 @@ import { readJSONFile, writeJSONFile } from './read-and-write-files.util'
 
 const filePath = 'path/to/your/file.txt'
 const data = { mock: 'content' }
+const invalidJSON = '< { }'
 const mockFileContent = JSON.stringify(data)
+
+beforeEach(() => {
+  const consoleErrorSpy = jest.spyOn(console, 'error')
+  consoleErrorSpy.mockImplementation(jest.fn())
+})
 
 describe('readJSONFile', () => {
   it('reads and returns the contents of a JSON file', async () => {
@@ -15,6 +21,20 @@ describe('readJSONFile', () => {
     expect(result).toStrictEqual(JSON.parse(mockFileContent))
     expect(fs.readFile).toHaveBeenCalledWith(filePath, 'utf8')
   })
+  it('handles ENOENT errors', async () => {
+    const expectedError = new Error('File not found')
+    ;(fs.readFile as jest.MockedFunction<typeof fs.readFile>).mockRejectedValue(
+      expectedError
+    )
+    await expect(readJSONFile(filePath)).rejects.toThrow(expectedError)
+    expect(fs.readFile).toHaveBeenCalledWith(filePath, 'utf8')
+  })
+  it('handles JSON parsing errors', async () => {
+    ;(fs.readFile as jest.MockedFunction<typeof fs.readFile>).mockResolvedValue(
+      invalidJSON
+    )
+    await expect(readJSONFile(filePath)).rejects.toThrow(SyntaxError)
+  })
 })
 
 describe('writeJSONFile', () => {
@@ -24,5 +44,14 @@ describe('writeJSONFile', () => {
     ).mockResolvedValue()
     const r = await writeJSONFile(filePath, data)
     expect(r).toBe(true)
+  })
+  it('handles errors', async () => {
+    const expectedError = new Error(
+      `Error: EACCES: permission denied, open '${filePath}`
+    )
+    ;(
+      fs.writeFile as jest.MockedFunction<typeof fs.writeFile>
+    ).mockRejectedValue(expectedError)
+    await expect(writeJSONFile(filePath, data)).rejects.toThrow(expectedError)
   })
 })
