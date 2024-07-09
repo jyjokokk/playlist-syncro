84e26792dd808122da6b0b5644cb776464f7093b
Jyrki Kokkola <jyrki.kokkola@outshine.fi>
2024-07-09 03:48:02 +0300

    +database service

---
+++ src/database/sqlite3-setup.ts
@@ -0,0 +1,21 @@
+import sqlite3 from 'sqlite3'
+import configService from '../config/config.service'
+
+const SQLITE_PATH = configService.getConfig().SQLITE_PATH
+
+export const setupDatabase = async () => {
+  const db = new sqlite3.Database(
+    SQLITE_PATH,
+    sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+    (err) => {
+      if (err) {
+        console.error(err.message)
+      }
+      console.log('Connected to the database.')
+    }
+  )
+  db.serialize(() => {
+    db.run('CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT)')
+  })
+  return db
+}
